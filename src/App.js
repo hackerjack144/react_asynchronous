80d2dc499eeb836f0b61a43708ebd6653273cb6f
hackjack123 <jaiswal.dheeraj123@gmail.com>
2020-04-28 18:17:23 +0530

    added code for react asynchronous sample

--- src/App.js
+++ src/App.js
@@ -2,25 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+constructor(props){
+  super(props)
+
+  this.state = {
+    meaningOfLife: 47 + this.props.increment
+  }
+}
+
+handleClick = () => {
+  this.setState((prevState, prevProps) =>{
+        return  {meaningOfLife: prevState.meaningOfLife + prevProps.increment}
+      }, 
+    () => console.log(this.setState.meaningOfLife))
+}
+
+render(){
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {this.state.meaningOfLife}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button
+          onClick = {this.handleClick}>
+          Update State
+        </button>
       </header>
     </div>
   );
 }
-
+}
 export default App;
